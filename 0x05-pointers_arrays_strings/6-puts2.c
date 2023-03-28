d2aa4614601cd868b17c9786d1be2ef9eddf1176
Arewa01 <omotosojoy@gmail.com>
2023-03-28 09:40:46 -0700

    fixed bug for task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,19 +1,24 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character of a string
- * @str: Input string
+ * puts2 - prints one char out of 2 of a string
+ * followed by a new line
+ * @str: string to print the chars from
  */
-
 void puts2(char *str)
 {
-	int i = 0;
+	int len, i;
 
-	while (str[i] != '\n')
+	len = 0;
+
+	while (str[len] != '\0')
 	{
-		if (i % 2 == 0)
-			_putchar(str[i]);
-		i++;
+		len++;
+	}
+
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
 	}
 
 	_putchar('\n');
