faac52a61fa6bc1255adc39851f335b0b4086726
Tosho Joy <omotosojoy@gmail.com>
2023-04-14 14:57:34 +0100

    a program that prints its name, followed by a new line

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,9 +4,10 @@
 *main - prints the name of the program
 *@argc: the argument count
 *@argv: the argument array
-@Return: Always 0 (Success)
+*Return: Always 0 (Success)
 */
-int main(int argc _attribute_((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", *argv);
+	printf("%s\n", argv[0]);
+	return (0);
 }
