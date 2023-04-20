e76396f54b0f024155039b40b3e2352578e9ff69
Tosho Joy <omotosojoy@gmail.com>
2023-04-20 22:57:13 +0100

    All the files for the project

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -21,11 +21,14 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (ops[i].op && strcmp(ops[i].op, s)){
+	while (ops[i].op) 
+	{	
+
+		if (strcmp(ops[i].op, s) == 0)
+		{	
+			return (ops[i].f);
+		}
 		i++;
-	if (strcmp(ops[i].op, s) == 0)
-	{
-		return (ops[i - 1].f);
 	}
 	return (NULL);
 }
