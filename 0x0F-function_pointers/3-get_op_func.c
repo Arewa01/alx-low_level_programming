04fa72b65b6a071fbf7e5e0f7bb98979aa407892
Tosho Joy <omotosojoy@gmail.com>
2023-04-20 22:18:37 +0100

    Revert "All the file for the project"
    This reverts commit e624e6ec1ec86b36f87b630785b6de0bcb4aea31.

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,10 +1,10 @@
 #include "3-calc.h"
 #include <string.h>
 /**
-* get_op_func - Returns a pointer to the function that corresponds
+* get_op_func - Returns a pointer to the function that corresponds 
 * to the operator given as a parameter.
 * @s: The operator passed as argument to the program
-* Return: a pointer to the function that corresponds to the operator
+* Return: a pointer to the function that corresponds to the operator 
 * given as a parameter
 */
 
@@ -28,7 +28,7 @@ int (*get_op_func(char *s))(int, int)
 	}
 	if (strcmp(ops[i].op, s) == 0)
 	{
-		return (ops[i - 1].f);
-	}
+		return ops[i-1].f;
+	}	
 	return (NULL);
-}
+ }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,50 +1,27 @@
 #include "3-calc.h"
 /**
-* op_add - a function that performs the sum of a and b
-* @a: first number
-* @b: second number
-* Return: the sum of a and b
+* @op_add: returns the sum of a and b
+* @op_sub: returns the difference of a and b
+* @op_mul: returns the product of a and b
+* @op_div: returns the result of the division of a by b
+* @op_mod: returns the remainder of the division of a by b
 */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
-/**
- * op_sub - a function that performs the subtraction of b from a
- * @a: first number
- * @b: second number
- * Return: the difference of a and b
- */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
-/**
- * op_mul - a function that performs the multiplication of a and b
- * @a: first number
- * @b: second number
- * Return: the product of a and b
- */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
-/**
- * op_div - a function that performs the division of a and b
- * @a: first number
- * @b: second number
- * Return: the division of a and b
- */
 int op_div(int a, int b)
 {
 	return (a / b);
 }
-/**
- * op_mod - a function that performs the modular division of a and b
- * @a: first number
- * @b: second number
- * Return: the modulus of a and b
- */
 int op_mod(int a, int b)
 {
 	return (a % b);
