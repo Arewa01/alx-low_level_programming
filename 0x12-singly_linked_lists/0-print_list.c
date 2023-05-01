0d94401339dc394a6c87ed317bde83ee855cd7ef
Tosho Joy <omotosojoy@gmail.com>
2023-05-01 15:57:36 +0100

    fixed bug on task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,6 +10,7 @@ size_t print_list(const list_t *h)
 	size_t c = 0;
 
 	while (h != NULL)
+	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
@@ -17,6 +18,5 @@ size_t print_list(const list_t *h)
 		c++;
 		h = h->next;
 	}
-
-		return (c);
+	return (c);
 }
