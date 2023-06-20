10ea0cac7ce9cbd083a4fb4b62507ba835ae0fa8
Tosho Joy <omotosojoy@gmail.com>
2023-06-20 17:44:51 +0100

    All the files in the project

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -23,5 +23,5 @@ void print_dog(struct dog *d)
 	{
 		printf("Age: (nil)");
 	}
-	printf("Name: %s\nAge: %2f\nOwner: %s\n", d->name, d->age, d->owner);
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
