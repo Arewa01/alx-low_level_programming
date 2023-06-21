d2c7e6a4610200b0943fc41e359466b649b47593
Tosho Joy <omotosojoy@gmail.com>
2023-06-21 12:20:47 +0100

    All the files in the project

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include "3-calc.h"
+/**
+ * op_add - gives the sum of a and b
+ * @a: first number
+ * @b: second number
+ * Return: the sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - gives the difference between a and b
+ * @a: first number
+ * @b: second number
+ * Return: the difference between a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - gives the multiplication of a and b
+ * @a: first number
+ * @b: second number
+ * Return: the product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - returns the division of a and b
+ * @a: first number
+ * @b: second number
+ * Return: the division of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - returns the modular division of a and b
+ * @a: first number
+ * @b: second number
+ * Return: the modular division of a and b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
