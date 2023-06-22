9445f742d9e090e6276a4352ec9a55e5d93349e0
Tosho Joy <omotosojoy@gmail.com>
2023-06-22 22:21:30 +0100

    all the files of the project

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,8 +7,7 @@
  */
 void print_all(const char * const format, ...)
 {
-	char *string;
-	char *seperator;
+	char *string, *separator = "";
 	int i = 0;
 	va_list args;
 
@@ -21,26 +20,26 @@ void print_all(const char * const format, ...)
 			switch (format[i])
 			{
 				case 'c':
-					printf("%s%c", seperator, va_arg(args, int));
+					printf("%s%c", separator, va_arg(args, int));
 					break;
 				case 'i':
-					printf("%s%d", seperator, va_arg(args, int));
+					printf("%s%d", separator, va_arg(args, int));
 					break;
 				case 'f':
-					printf("%s%f", seperator, va_arg(args, double));
+					printf("%s%f", separator, va_arg(args, double));
 					break;
 				case 's':
 					string = va_arg(args, char *);
 					if (!string)
 						string = "(nil)";
-					printf("%s%s", seperator, string);
+					printf("%s%s", separator, string);
 					break;
 
 				default:
 					i++;
 					break;
 			}
-			seperator = ", ";
+			separator = ", ";
 			i++;
 		}
 	}
