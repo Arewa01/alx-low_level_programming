a8b0ad30e15fd3d87383da98221ab2cfb5e48e74
Tosho Joy <omotosojoy@gmail.com>
2023-07-05 12:02:41 +0100

    All the files for the More Singly Linked Lists project

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,28 +1,23 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * add_node - a function that adds a new node
- *		at the beginning of a list_t list.
+ * add_nodeint - a function that adds a new node
+ *		at the beginning of a listint_t list.
  * @head: pointer to the list_t
- * @str: string to be duplicated
+ * @n: number to be duplicated
  * Return: the address of the new element, or NULL if it failed
  */
-list_t *add_node(list_t **head, const char *str)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
-	list_t *ptr = NULL;
-	unsigned int len = 0;
+	listint_t *ptr;
 
-	while (str[len])
-		len++;
-
-	ptr = malloc(sizeof(list_t));
+	ptr = malloc(sizeof(listint_t));
 	if (!ptr)
 		return (NULL);
 
-	ptr->str = strdup(str);
-	ptr->len = len;
+	ptr->n = n;
 	ptr->next = (*head);
 
 	(*head) = ptr;
-	return (*head);
+	return (ptr);
 }
--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,26 +1,22 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * add_node_end - a function that adds a new node at the end of a list_t list.
- * @head: pointer to the list_t
- * @str: string to be duplicated
+ * add_nodeint_end - a function that adds a new node
+ * at the end of a listint_t list.
+ * @head: pointer to the listint_t
+ * @n: number to be duplicated
  * Return: the address of the new element, or NULL if it failed
  */
-list_t *add_node_end(list_t **head, const char *str)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	list_t *ptr = NULL;
-	list_t *temp = *head;
-	unsigned int len = 0;
+	listint_t *ptr = NULL;
+	listint_t *temp = *head;
 
-	while (str[len])
-		len++;
-
-	temp = malloc(sizeof(list_t));
+	ptr = malloc(sizeof(listint_t));
 	if (!ptr)
 		return (NULL);
 
-	ptr->str = strdup(str);
-	ptr->len = len;
+	ptr->n = n;
 	ptr->next = NULL;
 
 	if (*head == NULL)
@@ -29,7 +25,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (ptr);
 	}
 
-	while (temp->next)
+	while (temp->next != NULL)
 	{
 		temp = temp->next;
 	}
