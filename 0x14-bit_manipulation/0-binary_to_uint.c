d19fcbcb5a104c7678c258899b573d9ccbc3cd55
Tosho Joy <omotosojoy@gmail.com>
2023-07-07 17:33:01 +0100

    All the files for the More Singly Linked Lists project

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,4 @@
 #include "main.h"
-
-unsigned int _strlen(const char *s);
 /**
  * binary_to_uint - a function that converts a binary number
  * @b: the string that contains the binary numbers
@@ -10,10 +8,12 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int i;
 	unsigned int decimal = 0;
-	unsigned int length = _strlen(b);
+	unsigned int length = 0;
 
 	if (b == NULL)
 		return (0);
+	while (b[length] != '\0')
+		length++;
 	for (i = 0; i < length; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
@@ -21,21 +21,4 @@ unsigned int binary_to_uint(const char *b)
 		decimal += (b[i] - '0') * (1 << (length - i - 1));
 	}
 	return (decimal);
-}
-/**
- * _strlen - Returns the length of a string
- * @s: The string character
- * Return: The character length
- */
-unsigned int _strlen(const char *s)
-{
-	int len = 0;
-
-	while (*s != '\0')
-	{
-		len++;
-		s++;
-	}
-
-	return (len);
 }
