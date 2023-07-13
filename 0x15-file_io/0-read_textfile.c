52c9baad082e4bbcc593595fe566f4ad722f5845
Tosho Joy <omotosojoy@gmail.com>
2023-07-13 14:39:46 +0100

    All the files for the More Singly Linked Lists project

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * read_textfile - a function that reads a text file.standard output.
+ * @filename:  the name of the file
+ * @letters: number of letters it shpuld read or print
+ * Return: the actual number of letters or 0 if it fails.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t rd, wr;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+	fd = open("filename", O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+
+	if (buffer == NULL)
+		return (0);
+	rd = read(fd, buffer, letters);
+	wr = write(STDOUT_FILENO, buffer, rd);
+
+	if (wr == -1 || rd == -1 || wr != rd)
+	{
+		free(buffer);
+		return (0);
+	}
+	free(buffer);
+	close(fd);
+	return (wr);
+}
