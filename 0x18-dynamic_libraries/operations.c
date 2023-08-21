aec59153d345a685fba841b477e59fe0da094fb1
Tosho Joy <omotosojoy@gmail.com>
2023-08-21 16:15:00 +0100

    Task 2

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+/**
+ * add - the sum of a and b
+ * @a: the first number
+ * @b: the second number
+ * Return: the sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - the difference of a and b
+ * @a: the first number
+ * @b: the second number
+ * Return: the difference of a and b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - the product of a and b
+ * @a: the first number
+ * @b: the second number
+ * Return: the product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - the quotient of a and b
+ * @a: the first number
+ * @b: the second number
+ * Return: the quotient of a and b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - the modulus of a and b
+ * @a: the first number
+ * @b: the second number
+ * Return: the modulus of a and b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
